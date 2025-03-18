1c1ddf614f2db8c283b2d9eab260accb39b48988
alessiozaccato <alessiozaccato@gmail.com>
2025-03-18 12:08:37 +0100

    ADD:milestone 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,41 @@
-export default function HomePage() {
-    return (
-        <h1 className="text-center">Bool Movies</h1>
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+export default function HomePage() {
+
+    const [movies, setmovies] = useState([]);
+
+    const fetchMovies = () => {
+        axios
+            .get('http://localhost:3000/movies')
+            .then((res) => {
+                setmovies(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    useEffect(fetchMovies, []);
+
+
+    return (
+        <>
+            <h1 className="text-center">Bool Movies</h1>
+            <div className='row'>
+
+                {
+                    movies.map((movie) => {
+                        return (
+                            <div key={movie.id} className='col'>
+                                <p>{movie.title}</p>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+
+        </>
     )
 }
